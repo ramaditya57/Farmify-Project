5df990787e992799618b258c0809ff29acd40aa7
ramaditya57 <ramadityachaudhary@gmail.com>
2025-05-07 09:06:45 +0530

    Updated

--- app.py
+++ app.py
@@ -171,4 +171,7 @@ def get_history_proxy():
 
 # ==================== Run App ====================
 if __name__ == '__main__':
-    app.run(debug=True, port=5003)
+    # app.run(debug=True, port=5003)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
